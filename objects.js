d00ee465f27415543966055c4fe58a2f14aae4d2
Daniel Killoran <dankilloran@gmail.com>
2019-03-22 01:24:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,14 +1,14 @@
 var playlist = {
   Beyonce: 'Formation',
   War: 'Low Rider'
-};
+}
 
 var updatePlaylist = function(playlist, Sublime, Santeria) {
   playlist[Sublime] = Santeria
   return playlist
-};
+}
 
 var removeFromPlaylist = function(playlist, Beyonce) {
   delete playlist.War;
   return playlist
-};
+}
